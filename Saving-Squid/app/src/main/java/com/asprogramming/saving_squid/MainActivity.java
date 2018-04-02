944d0b46541157bba67989ea39228c8d7b6b7984
Jasonkel <jasonkelly85@live.ie>
2018-04-02 17:18:57 +0100

    Android Project Created

---
+++ Saving-Squid/app/src/main/java/com/asprogramming/saving_squid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.asprogramming.saving_squid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
